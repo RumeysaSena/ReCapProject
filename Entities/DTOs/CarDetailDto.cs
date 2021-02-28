b4e81f837e55a70e511b378d5842cf1bf54b218b
Rumeysa Sena Özdemir <77540535+ozrumsena@users.noreply.github.com>
2021-02-28 12:59:04 +0300

    ...

--- Entities/DTOs/CarDetailDto.cs
+++ Entities/DTOs/CarDetailDto.cs
@@ -8,9 +8,9 @@ namespace Entities.DTOs
     public class CarDetailDto : IDto
     {
         public int Id { get; set; }
-        public int CarName { get; set; }
-        public int BrandName { get; set; }
-        public int ColorName { get; set; }
+        public string CarName { get; set; }
+        public string BrandName { get; set; }
+        public string ColorName { get; set; }
         public decimal DailyPrice { get; set; }
         
     }
